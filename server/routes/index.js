10c7e983f28e44d0d36ca53eb6669e3eca6bfab2
MaxAllen <maxton.allen@gmail.com>
2017-08-16 17:42:33 -0400

    added get route

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,14 @@ var router = require("express").Router();
 var Hotel = require("../models").Hotel;
 var Restaurant = require("../models").Restaurant;
 var Activity = require("../models").Activity;
+var Itinerary = require("../models").Itinerary;
+
+router.get("/api/itineraries/:itinerary_id", (req,res,next) => {
+  Itinerary.findAll({ include: [{ all: true }], where:{id: req.params.itinerary_id}}).then(function(itinerary){
+    console.log(itinerary)
+    res.json(itinerary) 
+  })
+})
 
 router.get("/", (req, res, next) => {
   Promise.all([
@@ -18,5 +26,6 @@ router.get("/", (req, res, next) => {
     })
     .catch(next);
 });
+
 
 module.exports = router;
